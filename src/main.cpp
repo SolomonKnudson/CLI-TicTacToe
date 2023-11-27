5b2477b62829a65b5b7a0f4e9ce904a870056acd
Solomon Knudson <solomonknudson@gmail.com>
2023-11-27 03:33:25 -0500

    Fixed bug in clearWinConFig util methods
    clearWinConFig util methods would assign an int into a board piece slot instead of BoardPiece.m_piecePostion. Causing a new board piece to be constructed making the methods appear to be working correctly.  They now call reset() on the board piece to properly reset it without losing the piece position on the board, or creating an entirly
    new board slot with the same position.

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,13 @@ int main(int argc, char *argv[])
 {
     char playerX{ 'X'};
     char playerO{ 'O'};
-    Board testBoard{};
+    Board testBoard{55, 55};
     testBoard.display();
-    testBoard.setDiagonalWin(1, playerX);
+    //testBoard.setDiagonalWin(12, playerX);
+    //testBoard.setVerticalWin(12, playerX);
+    testBoard.setLateralWin(166, playerX);
     testBoard.display();
-    testBoard.clearWinConfiguration(playerX);
+    testBoard.clearWinConfigurationB(playerX);
     testBoard.display();
     TicTacToe ticTacToe{};
     return 0;
