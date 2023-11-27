edaeb835fe94f67ec7b4438628008b315e829ac4
Solomon Knudson <solomonknudson@gmail.com>
2023-11-26 23:29:36 -0500

    Created new branch
    Created new branch to refactor Boards internal table structure while keeping a working version intact.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include "tictactoe.h"
 
+struct BoardPiece
+{
+    int piecePosition{};
+    char playerFlag{};
+    bool playerIsOccupying{};
+};
+
+
+
 int main(int argc, char *argv[])
 {
+    int playerX{ -1 };
+    int playerO{ -2 };
+    Board testBoard{ 5, 5 };
+    testBoard.display(playerX, playerO);
+    testBoard.setDiagonalWin(1, playerX);
+    testBoard.display(playerX, playerO);
+    testBoard.clearWinConfiguration(playerX);
+    testBoard.display(playerX, playerO);
     TicTacToe ticTacToe{};
     return 0;
 }
