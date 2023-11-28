8be9587abfc1c0fb1f9771acbeb30b2b833bdc64
Solomon Knudson <solomonknudson@gmail.com>
2023-11-27 22:00:48 -0500

    Change if checks to if else

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include "tictactoe.h"
-#include "board.h"
-#include <vector>
-
-
 
 int main(int argc, char *argv[])
 {
+    char playerX{ 'X' };
+    char playerO{ 'O' };
+    Board testBoard{35, 45};
+    testBoard.display();
+    testBoard.setDiagonalWin(10, playerX);
+    testBoard.display();
+    testBoard.clearWinConfiguration(playerX);
+    testBoard.display();
     TicTacToe ticTacToe{};
     return 0;
 }
