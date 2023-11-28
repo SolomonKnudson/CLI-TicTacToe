7005ea2279e6ca9ddb115831a17e2de564475e15
Solomon Knudson <solomonknudson@gmail.com>
2023-11-28 02:56:15 -0500

    remove debug code from main

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,6 @@
 
 int main(int argc, char *argv[])
 {
-    char playerX{ 'X' };
-    char playerO{ 'O' };
-    Board testBoard{35, 45};
-    testBoard.display();
-    testBoard.setDiagonalWin(10, playerX);
-    testBoard.display();
-    testBoard.clearWinConfiguration(playerX);
-    testBoard.display();
     TicTacToe ticTacToe{};
     return 0;
 }
