41244a3178686a6596bc78feee8e6224cd44dcf4
Solomon Knudson <solomonknudson@gmail.com>
2023-12-08 12:01:38 -0500

    Add starter code
    added starter code for testing different board sizes

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
+#include <iostream>
 #include "tictactoe.h"
+#include "board.h"
 
 int main(int argc, char *argv[])
 {
-    Board testBoard{20, 5};
+    //Starter code for testing different board sizes
+    char playerO{ 'O' };
+    char playerX{ 'X' };
+
+    Board testBoard{4, 12};
+    testBoard.display();
+
+    testBoard.setDiagonalWin(8, playerX, true);
+    testBoard.display();
+
+    if (testBoard.isWinningMove(playerX))
+    {
+        std::cout << "|Win Case: " << testBoard.winCase() << '\n';
+    }
+
+    testBoard.clearWinConfiguration(playerX);
     testBoard.display();
 
     TicTacToe ticTacToe{};
